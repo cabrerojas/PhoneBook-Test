662e8f7552232fc95da171b5bfe890dc9606376e
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-06-09 00:04:02 -0400

    lazyload
    se implementa el lazyload para el modulo de usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'users',
+    loadChildren: () => import('./users/users.module').then( m => m.UsersModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: '**',
+    redirectTo: 'users'
+  },
 ];
 
 @NgModule({
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children:[
+      { path: 'list', component: UserListComponent },
+      { path: 'user/:id', component: UserDetailComponent },
+      { path: '**', redirectTo: 'list' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
